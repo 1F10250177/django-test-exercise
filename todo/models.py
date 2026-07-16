c74e5996145aaccf36cf0722c0d8c84d66e64244
s1F10240055 <s1F102400550@iniad.org>
2026-07-16 11:33:17 +0900

    ログイン機能

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 
 
 # Create your models here.
 class Task(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        null=True,
+        blank=True,
+    )
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
